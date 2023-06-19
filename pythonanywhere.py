35d2fe94249eef8016c9bf1865381e83a5714456
ja <ja@zdb.es>
2023-06-19 17:46:15 +0200

    primera version

---
+++ pythonanywhere.py
@@ -0,0 +1,15 @@
+import requests
+username = 'jabatron'
+token = '9c1524e44274ac843b17d3d45d8a099ecc07bb00'
+
+response = requests.get(
+    'https://www.pythonanywhere.com/api/v0/user/{username}/cpu/'.format(
+        username=username
+    ),
+    headers={'Authorization': 'Token {token}'.format(token=token)}
+)
+if response.status_code == 200:
+    print('CPU quota info:')
+    print(response.content)
+else:
+    print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
